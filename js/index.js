83326bb970012e4dc203a5eed14f7633e5e655cb
Gabriela <gabriela@dyn-160-39-254-220.dyn.columbia.edu>
2019-01-17 10:59:28 -0500

    First website

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// $('h3').on('mouseover', function() {
+//   $(this).animate({
+//     "letter-spacing": "10px",
+//     "font-size": "40px"
+//   }, 1000);
+// });
+$(document).ready(function() {
+  console.log("HEYY");
+  // $('h1').on('click', function() {
+  //   $(this).html('wow lol');
+  // });
+});
